037558364a7754dc8305a3cf24004f5e38a075ca
ronan clancy <rjclancy@users.noreply.github.com>
2014-02-08 22:32:36 +0000

    Update Program.cs

--- Kidnaper/Program.cs
+++ Kidnaper/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -10,8 +10,9 @@ namespace Kidnaper
         static void Main()
         {
             const string note = "I want your money";
-            const string paper = "Management at Tallaght Hospital say they plan to increase staffing levels at the facility in an effort to deal with overcrowding.51 mins agoPSNI arrest three amid chaos at Hardwell gig in BelfastPSNI chief inspector Mark McEwan has said a small quantity of suspected drugs was seized outside the Odyssey Arena in the wake of a major incident that saw 17 teenagers taken to hospital.1 hour, 10 mins ago";
-
+            const string paper = "Shortly before Christmas, people Iin Athlone needlessly paid hundredI want your moneys of euro iwantn parking fees because theyour y were not aware moneyof a ‘freeI want your monyer";
+           
+            //Time method execution
             //start stopwatch
             var watch = Stopwatch.StartNew();
 
@@ -28,36 +29,30 @@ namespace Kidnaper
 
         //check note method(Regular)
         private static void CheckNote(string note, string paper)
-        {
-            var noteSet = new HashSet<char>(note);
-            var paperSet = new HashSet<char>(paper);
+        {           
+            var noteList = new List<char>(note);
+            var paperList = new List<char>(paper);
             var letterSuccess = 0;
-            var flag = true;
 
             //note interation
-            for (var i = 0; i < noteSet.Count; i++)
+            foreach (var i in noteList)
             {
-                do
-                {                   
-                    if (!paperSet.Contains(noteSet.ElementAt(i))) continue;
+                if (!paperList.Contains(i)) continue;
 
-                    //increment successfull letter found, then remove letter from paperSet
-                    letterSuccess++;
-                    paperSet.Remove(noteSet.ElementAt(i));
-                    flag = false;
-                } while (flag);
+                //increment successfull letter found, reset paperChar letter
+                letterSuccess++;
+                paperList.Remove(i);
             }
 
             //check letters count
-            if (letterSuccess == noteSet.Count)
+            if (letterSuccess == noteList.Count)
             {
-                Console.WriteLine("Success! Enough letters to create : " + note + "!");
+                Console.WriteLine("Success!!! Enough letters to create : " + note + "!");
             }
             else
             {
-                Console.WriteLine("Not enough letters!" + note + "!");
-            }           
+                Console.WriteLine("Not enough letters to create : " + note + "!");
+            }
         }
     }
 }
-
